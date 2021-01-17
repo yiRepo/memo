78dca0597bb5d84e4b7aa54ad564de03ddc7b889
yojiro isobe <yoisobe@outlook.com>
2021-01-17 16:11:22 +0900

    first commit

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Memo
+
+
+class MemoForm(ModelForm):
+    class Meta:
+        model = Memo
+        fields = ['title','text']
---
+++ app/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path,include
+from . import views
+
+
+
+
+urlpatterns = [
+
+ path('',views.index,name="index"),
+ path('<int:memo_id>',views.detail,name='detail'),
+ path('new_memo',views.new_memo,name='new_memo'),
+ path('delete_memo/<int:memo_id>',views.delete_memo,name='delete_memo'),
+ path('edit_memo/<int:memo_id>',views.edit_memo,name='edit_memo'),
+
+]
